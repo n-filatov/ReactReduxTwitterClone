4fcb9f5d705b0771367d888b09d9dca8182f74ec
Nikita <keddofilatov@gmail.com>
2017-06-10 21:46:00 +0600

    [+] header styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import './icons.css';
 import Header from './Components/Header';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 class App extends Component {
   render() {
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
+import './header.css';
+import './navigation-button.css';
+import './search-from.css';
 
 const Header = () => {
+  const userAvatarUrl = 'https://pbs.twimg.com/profile_images/787155097159688192/iWYsZVlM_normal.jpg';
   return (
-    <div className="header">
-      <div className="header__left-bar">
-        <a href="" className="header__navigation-button navigation-button">
-          <i className="navigation-button__icon"></i>
-          <span className="navigation-button__text"></span>
-        </a>
-        <a href="" className="header__navigation-button navigation-button">
-          <i className="navigation-button__icon"></i>
-        </a>
-        <a href="" className="header__navigation-button navigation-button">
-          <i className="navigation-button__icon"></i>
-          <span className="navigation-button__text"></span>
-        </a>
+    <div className="header__wrapper">
+      <div className="header">
+        <div className="header__left-bar">
+          <a href="" className="header__navigation-button navigation-button">
+            <i className="navigation-button__icon navigation-button__icon--home glyphicon glyphicon-home"></i>
+            <span className="navigation-button__text">Главная</span>
+          </a>
+          <a href="" className="header__navigation-button navigation-button">
+            <i className="navigation-button__icon glyphicon glyphicon-bell"></i>
+            <span className="navigation-button__text">Уведомления</span>
+          </a>
+          <a href="" className="header__navigation-button navigation-button">
+            <i className="navigation-button__icon glyphicon glyphicon-envelope"></i>
+            <span className="navigation-button__text">Сообщения</span>
+          </a>
+        </div>
+        <i className="twitter-icon"></i>
+        <div className="header__right-bar">
+          <form action="/search" className="header__search-form search-from">
+            <input type="text" className="search-form__input" placeholder="Поиск в Твиттере"/>
+          </form>
+          <img src={ userAvatarUrl } alt="" className="header__user-avatar"/>
+          <button className="header__button">Твитнуть</button>
+        </div>
       </div>
-      <i className="twitter-icon"></i>
-      <div className="header__right-bar"></div>
     </div>
   )
 }
